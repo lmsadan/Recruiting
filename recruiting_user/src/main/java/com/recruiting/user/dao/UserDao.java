2d0e1ddcc53286d3976ee462d9556f45db4f7f5f
lzq_pro <lzq520821@qq.com>
2020-05-09 11:08:51 +0800

    dabian

--- recruiting_user/src/main/java/com/recruiting/user/dao/UserDao.java
+++ recruiting_user/src/main/java/com/recruiting/user/dao/UserDao.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import com.recruiting.user.pojo.User;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 数据访问接口
@@ -15,10 +16,13 @@ import org.springframework.data.jpa.repository.Query;
 public interface UserDao extends JpaRepository<User,String>,JpaSpecificationExecutor<User>{
 	User findByMobile(String mobile);
 
+	@Transactional
     @Modifying
 	@Query(value = "update tb_user set fanscount = fanscount+? where id=?",nativeQuery = true)
     void updatefanscount(int x, String friendid);
 
+
+    @Transactional
     @Modifying
     @Query(value = "update tb_user set followcount = followcount+? where id=?",nativeQuery = true)
     void updatefollowcount(int x, String userid);
